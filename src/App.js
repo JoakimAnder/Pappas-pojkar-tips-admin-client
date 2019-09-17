75863ddcf783594c6e22c049ccdc0801782e168e
Joakim Andersson <Joakim.Ander@hotmail.com>
2019-09-17 20:06:42 +0200

    Basic stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Main from "./components/Main";
+import Login from "./components/Login";
+
 
 function App() {
+  const [page, setpage] = useState("login");
+  function parsePage() {
+    switch (page) {
+      case "main":
+        return <Main />
+      default:
+        return <Login />
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      {parsePage()}
     </div>
   );
 }
